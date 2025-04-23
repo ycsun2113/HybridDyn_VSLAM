a68852406f3b0f9f5d15f111fd14e2a1e06d9aeb
ycs <ycs@umich.edu>
2025-04-22 22:50:16 -0400

    update

---
+++ ORB_SLAM3/Examples/RGB-D/rgbd_tum_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pathDatasetTUM='/Datasets/tum_rgbd' #Example, it is necesary to change it by the dataset path
+
+./rgbd_tum ../../Vocabulary/ORBvoc.txt ./TUM3.yaml "$pathDatasetTUM"/rgbd_dataset_freiburg3_walking_halfsphere "$pathDatasetTUM"/rgbd_dataset_freiburg3_walking_halfsphere/fr3_w_half.txt
