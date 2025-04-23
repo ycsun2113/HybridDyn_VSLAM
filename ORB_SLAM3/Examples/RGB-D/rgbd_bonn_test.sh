44bebf51dc56a13722720b23fcca6a5127f60837
ycs <ycs@umich.edu>
2025-04-22 21:36:44 -0400

    update

--- ORB_SLAM3/Examples/RGB-D/rgbd_bonn_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pathDatasetBonn='/Datasets/bonn_rgbd' #Example, it is necesary to change it by the dataset path
-
-./rgbd_tum ../../Vocabulary/ORBvoc.txt ./TUM3.yaml "$pathDatasetBonn"/rgbd_bonn_moving_nonobstructing_box "$pathDatasetBonn"/rgbd_bonn_moving_nonobstructing_box/moving_no_box.txt
--- ORB_SLAM3/evaluation/eval_tum_ate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pathDatasetTUM='/Datasets/tum_rgbd' #Example, it is necesary to change it by the dataset path
-
-python3 evaluate_ate_tum.py "$pathDatasetTUM"/rgbd_dataset_freiburg3_walking_static/groundtruth.txt ../Examples/RGB-D/results/fr3_w_static.txt --plot ./plots/fr3_w_static.pdf --verbose
-
